e48dac67fc0c7ec19f6416388045376fe4b9821a
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-10-16 11:53:19 +0530

    made start game UI richer and faster

--- src/com/sensei/mines/ui/newgame/NewGame.java
+++ src/com/sensei/mines/ui/newgame/NewGame.java
@@ -9,11 +9,21 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class NewGame extends Stage {
+	
+	private enum SettingsType {
+		BEGINNER, INTERMEDIATE, EXPERT, CUSTOM
+	}
 	
 	@FXML
 	public TextField numCols;
@@ -21,6 +31,17 @@ public class NewGame extends Stage {
 	public TextField numRows;
 	@FXML
 	public TextField numMines;
+	
+	@FXML
+	public RadioButton beginnerButton = null;
+	@FXML
+	public RadioButton intermediateButton = null;
+	@FXML
+	public RadioButton expertButton = null;
+	@FXML
+	public RadioButton customButton = null;
+	
+	private ToggleGroup group = null;
 	
 	private Mines application = null;
 	
@@ -34,54 +55,102 @@ public class NewGame extends Stage {
 		
 		Scene s = new Scene( ap );
 		super.setTitle( "New Game" );
+		if( application.getStage() != null ) {
+			this.initModality( Modality.APPLICATION_MODAL );
+			this.initOwner( application.getStage() );
+		}
 		super.setScene( s );
 	}
 	
 	@FXML
 	public void initialize() {
-		numCols.textProperty().addListener(new ChangeListener<String>() {
+		
+		ChangeListener<String> c = new ChangeListener<String>() {
 	        @Override
 	        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 	            if (!newValue.matches("\\d*")) {
 	                numCols.setText(newValue.replaceAll("[^\\d]", ""));
 	            }
 	        }
-	    });
-		numRows.textProperty().addListener(new ChangeListener<String>() {
-	        @Override
-	        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-	            if (!newValue.matches("\\d*")) {
-	                numRows.setText(newValue.replaceAll("[^\\d]", ""));
-	            }
-	        }
-	    });
-		numMines.textProperty().addListener(new ChangeListener<String>() {
-	        @Override
-	        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-	            if (!newValue.matches("\\d*")) {
-	                numMines.setText(newValue.replaceAll("[^\\d]", ""));
-	            }
-	        }
-	    });
+	    };
+		
+		numCols.textProperty().addListener( c );
+		numRows.textProperty().addListener( c );
+		numMines.textProperty().addListener( c );
+		
+		group = new ToggleGroup();
+		beginnerButton.setToggleGroup( group );
+		beginnerButton.setUserData( SettingsType.BEGINNER );
+		intermediateButton.setToggleGroup( group );
+		intermediateButton.setUserData( SettingsType.INTERMEDIATE );
+		expertButton.setToggleGroup( group );
+		expertButton.setUserData( SettingsType.EXPERT );
+		customButton.setToggleGroup( group );
+		customButton.setUserData( SettingsType.CUSTOM );
+		
+		group.selectToggle( beginnerButton );
 	}
 
 	@FXML
 	public void onStartButtonClicked() {
+		
+		Toggle t = group.getSelectedToggle();
+		SettingsType settings = (SettingsType)t.getUserData();
+		switch( settings ) {
+			case BEGINNER:
+				application.startGame( 10, 10, 10 );
+				this.hide();		
+			break;
+			
+			case INTERMEDIATE:
+				application.startGame( 16, 16, 40 );
+				this.hide();		
+			break;
+			
+			case EXPERT:
+				application.startGame( 16, 30, 99 );
+				this.hide();		
+			break;
+			
+			case CUSTOM:
+				startCustomGame();
+			break;			
+		}
+		
+	}
+	
+	private void showAlert( String alertText ) {
+		Alert alert = new Alert( AlertType.WARNING );
+		alert.setTitle( "Warning" );
+		alert.setHeaderText( null );
+		alert.setContentText( alertText );
+
+		alert.showAndWait();
+	}
+	
+	private void startCustomGame() {
 		try {
 			int cols = Integer.parseInt( numCols.getText() );
 			int rows = Integer.parseInt( numRows.getText() );
 			int mines = Integer.parseInt( numMines.getText() );
 			
-			if( mines > (rows*cols)/2 ) {
+			int mul = (rows*cols)/2;
+			int mll = (rows*cols)/11;
+			if( mines > mul || mines < mll ) {
 				numMines.setText( "" );
-				throw new Exception( "too many mines" );
+				throw new IllegalArgumentException( "mines need to be in the "
+						+ "range of " + mll + " to " + mul );
 			}
-			
+
 			application.startGame( rows, cols, mines );
-			this.hide();
-		} catch( Exception ex ) {
-			// swalla;
-		}
+			this.hide();		
+		} catch( NumberFormatException ex ) {
+			showAlert( "Enter a suitable value in the custom fields" );
+			
+		} catch( IllegalArgumentException ex ) {
+			showAlert( ex.getMessage() );
+		}				
+		
 	}
 	
 	@FXML
